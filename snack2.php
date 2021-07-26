4f7ec52ebe122a72c9a2b6358765a74c9f414c1e
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-26 16:05:29 +0200

    fixed all snack2

--- snack2.php
+++ snack2.php
@@ -11,17 +11,17 @@
 <!-- Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
   
-<a href="?name=Luca&mail=luca@mail.it&age=1">Add url data</a>
+<a href="?name=Luca&mail=luca@mail.it&age=30">Add url data</a>
 
-<?php
+<?php //prendo i valori dall' url e li assegno a variabili
    $name = $_GET['name'];
    $mail = $_GET['mail'];
    $age = $_GET['age'];
 
-   if (strlen($name) > 3 && 
-    strpos($mail, '@') != false &&
-    strpos($mail, '.') != false &&
-    is_numeric($age) != 0){
+   if (strlen($name) > 3 && //se $name è più lungo di 3 caratteri e
+    strpos($mail, '@') != false && //se mail contiene il carattere '@' (quando risponde false non è presente, altrimenti risponde con la posizione)
+    strpos($mail, '.') != false && //come sopra ma con il carattere '.'
+    is_numeric($age) == true){ //is_numeric trava se una variabile è un nuemro o una stringa numerica, se true = numero, false != numero
       $verifica = 'Accesso riuscito';
     } else {
       $verifica = 'Accesso negato';
@@ -29,12 +29,11 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
     var_dump($name);
     var_dump($mail);
     var_dump($age);
-    var_dump(is_numeric($age));
+    // var_dump(is_numeric($age));
 ?>
 
 <div>
   <?php
-    
     echo $verifica;
   ?>
 </div>
